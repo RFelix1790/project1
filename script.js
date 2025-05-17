37eb15a79da49049f4c24fadc06fe5392bd350ec
RFelix1790 <rmsfelix19@gmail.com>
2025-05-17 16:59:28 +0200

    basic html

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    const startButton = document.getElementById("start-button")
+    const gameContainer = document.getElementById("gameContainer")
+    const endGame= document.getElementById("endGame")
+
+startButton.addEventListener("click", () =>  {
+    startGame()
+})
+
+function startGame(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
