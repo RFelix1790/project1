fb2ae80368bfd01fc1199fba263e787fadaa1ad2
RFelix1790 <rmsfelix19@gmail.com>
2025-05-29 19:00:24 +0200

    test

--- js/game.js
+++ js/game.js
@@ -64,7 +64,7 @@ class Game {
       this.position[Math.floor(Math.random() * this.position.length)];
     if (place.isActivated === false) {
       const img = document.createElement("img");
-      img.src = "../styles/cowboy.PNG";
+      img.src = "../images/cowboy.PNG";
       img.alt = "Cowboy";
       img.style.position = "absolute";
       img.style.top = place.top + "px";
@@ -102,7 +102,7 @@ class Game {
       this.position[Math.floor(Math.random() * this.position.length)];
     if (place.isActivated === false) {
       const img = document.createElement("img");
-      img.src = "/styles/civilian.avif";
+      img.src = "../images/civilian.avif";
       img.alt = "civilian";
       img.style.position = "absolute";
       img.style.top = place.top + "px";
